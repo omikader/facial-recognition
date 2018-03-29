17bce5355f5fab602f324c1223242bd976a6cba5
Omar Abdelkader <omikader@gmail.com>
2018-03-29 13:49:29 -0400

    add tiebreaking rules for k_nn

--- code/k_nn.m
+++ code/k_nn.m
@@ -1,21 +1,26 @@
-function [ predictions ] = k_nn( k, training_data, testing_data )
+function [ predictions ] = k_nn(k, training_data, testing_data, tiebreaker)
 %K_NN Classifies each test sample based on the label of the majority of its
 %k nearest training sample neighbors.
-%   predictions = K_NN(k, training_data, testing_data) will return a matrix
-%   with the prediction for each test data point.
+%   predictions = K_NN(k, training_data, testing_data, tiebreaker) will
+%   return a matrix with the predictions for each test data point. 
+%
+%   -----------------------------------------------------------------------
+%   | Tiebreaker Mode | Description                                       |
+%   -----------------------------------------------------------------------
+%   | Discard         | Samples with a tie are discarded                  |
+%   | Retry           | Repeat with k-1 neighbors until the tie is broken |
+%   | Closest         | Choose the class with the minimum total distance  |
+%   | Random          | Choose the class randomly                         |
+%   -----------------------------------------------------------------------
 
 num_classes = size(testing_data, 3);
 num_training_samples_per_class = size(training_data, 2);
 num_testing_samples_per_class = size(testing_data, 2);
 
-if mod(k, 2) == 0
-    msg = 'Please select an odd value for ''k''.';
-    error(msg);
-end
-
 num_samples = num_classes * num_training_samples_per_class;
 if k > num_samples
-    msg = 'The value of ''k'' you have selected is larger than the number of potential samples: %d.';
+    msg = ['The value of ''k'' you have selected is larger than the' ...
+              'number of potential samples: %d.'];
     error(msg, num_samples);
 end
 
@@ -39,27 +44,79 @@ for i = 1:num_classes
         end
         
         % Sort the tuples by the distance entry
-        
         neighbors = sortrows(neighbors, 1);
         
         % Determine the class count for the k closest samples
-        
-        count = zeros(num_classes, 1);
+        count = zeros(num_classes, 2);
         for l = 1:k
+            distance = neighbors(l, 1);
             class = neighbors(l, 2);
-            count(class) = count(class) + 1;
+            count(class, 1) = count(class, 1) + 1;
+            count(class, 2) = count(class, 2) + distance;
         end
         
         % Assign the label of the largest class count
-        
-        max = 0;
+        majority = 0;
         for l = 1:num_classes
-            if count(l) > max
-                max = count(l);
+            if count(l, 1) > majority
+                tie = false;
+                majority = count(l, 1);
                 predictions(m, i) = l;
+            elseif count(l, 1) == majority
+                tie = true;
             end
         end
         
+        % In the event of a tie, use tiebreaker parameter
+        if tie
+            switch tiebreaker  
+                % If discard is chosen, assign label of -1
+                case 'discard'
+                    predictions(m, i) = -1;
+                    
+                % If retry is chosen, repeat algorithm with k-1 neighbors 
+                case 'retry'
+                    while tie
+                        k = k - 1;
+
+                        count = zeros(num_classes, 1);
+                        for l = 1:k
+                            class = neighbors(l, 2);
+                            count(class) = count(class) + 1;
+                        end
+
+                        majority = 0;
+                        for l = 1:num_classes
+                            if count(l) > majority
+                                tie = false;
+                                majority = count(l);
+                                predictions(m, i) = l;
+                            elseif count(l) == majority
+                                tie = true;
+                            end
+                        end
+                    end
+                    
+                % If distance is chosen, select the tied label with the
+                % minimum total distance
+                case 'closest'
+                    majority = max(count(:, 1));
+                    indices = find(count(:, 1) == majority);
+                    closest = min(count(indices, 2));
+                    predictions(m, i) = indices(count(indices, 2) == closest);
+                    
+                % If random is chosen, select one the tied labels randomly 
+                case 'random'
+                    majority = max(count(:, 1));
+                    indices = find(count(:, 1) == majority);
+                    predictions(m, i) = datasample(indices, 1);
+                 
+                % Throw an error if an invalid tiebreaker is selected    
+                otherwise
+                    msg = 'You have selected an invalid tiebreaker';
+                    error(msg);
+            end
+        end  
     end
 end
 
--- code/utils/get_accuracy.m
+++ code/utils/get_accuracy.m
@@ -2,18 +2,24 @@ function [ accuracy ] = get_accuracy( predictions, testing_data )
 %GET_ACCURACY Given the predictions and the testing data, calculate the
 %accuracy of the predictions.
 %   accuracy = GET_ACCURACY(predictions, testing_data) will return the
-%   correctness (between 0 and 1) of a given model's predictions
+%   correctness (between 0 and 1) of a given model's predictions.
 
 num_classes = size(testing_data, 3);
 num_samples_per_class = size(testing_data, 2);
 
 num_correct = 0;
+num_labeled = 0;
+
 for i = 1:num_classes
     for n = 1:num_samples_per_class
-        if predictions(n, i) == i
-            num_correct = num_correct + 1;
+        guess = predictions(n, i);
+        if guess ~= -1
+            num_labeled = num_labeled + 1;
+            if guess == i
+                num_correct = num_correct + 1;
+            end
         end
     end
 end
 
-accuracy = num_correct / (num_classes * num_samples_per_class);
+accuracy = num_correct / num_labeled;
