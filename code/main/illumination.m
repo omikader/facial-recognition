427b7366543642f112cea618c38173a3b56ef2e9
Omar Abdelkader <omikader@gmail.com>
2018-04-07 20:07:39 -0400

    add bayes/knn classification post-PCA

--- code/main/illumination.m
+++ code/main/illumination.m
@@ -40,6 +40,35 @@ k_nn_accuracy = get_accuracy(k_nn_predictions, testing_data);
 alpha = 0.05;
 W_pca = pca(training_data, alpha);
 
+% Project the original dataset onto the principal components
+
+training_proj = zeros(size(W_pca, 1), size(training_data, 2), size(training_data, 3));
+testing_proj = zeros(size(W_pca, 1), size(testing_data, 2), size(testing_data, 3));
+
+for j = 1:size(training_data, 3)
+    for k = 1:size(training_data, 2)
+        training_proj(:, k, j) = W_pca * training_data(:, k, j);
+    end
+end
+
+for j = 1:size(testing_data, 3)
+    for k = 1:size(testing_data, 2)
+        testing_proj(:, k, j) = W_pca * testing_data(:, k, j);
+    end
+end
+
+% Post PCA Bayesian Classification
+
+params = mle(training_proj, 'normal');
+bayesian_predictions = bayes(params, testing_proj, 'normal');
+bayesian_accuracy = get_accuracy(bayesian_predictions, testing_proj);
+
+% Post PCA K-NN Classification
+
+k = 1;
+k_nn_predictions = k_nn(k, training_proj, testing_proj, 'discard');
+k_nn_accuracy = get_accuracy(k_nn_predictions, testing_proj);
+
 %% Fisher's Multiple Discriminant Analysis (MDA)
 % Use Fisher's linear discriminant analysis technique (generalized for 'c'
 % classes) for dimensionality reduction.
