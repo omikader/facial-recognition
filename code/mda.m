40d525431cf82d155bf0e55164ff38587ab5e8b5
Omar Abdelkader <omikader@gmail.com>
2018-04-11 22:32:26 -0400

    use pseudoinverse instead of calculating gen eig problem

--- code/mda.m
+++ code/mda.m
@@ -9,25 +9,25 @@ num_features = size(training_data, 1);
 num_classes = size(training_data, 3);
 num_samples_per_class = size(training_data, 2);
 
-% S_w = zeros(num_features, num_features);
-% S_b = zeros(num_features, num_features);
-% m = mean(mean(training_data, 3), 2);
-% 
-% for i = 1:num_classes
-%     m_i = mean(training_data(:, :, i), 2);
-%     S_b = S_b + (num_samples_per_class * ((m_i - m) * (m_i - m)'));
-%     S_i = zeros(num_features, num_features);
-%     for n = 1:num_samples_per_class
-%         S_i = S_i + (training_data(:, n, i) - m_i) * (training_data(:, n, i) - m_i)';
-%     end
-%     S_w = S_w + S_i;
-% end
-% 
-% [V, D] = eig(S_b, S_w);
-% [~, ind] = sort(diag(D), 'descend');
-% Vs = V(:, ind);
-% 
-% transformation_matrix = Vs(:, 1:(num_classes - 1));
+S_w = zeros(num_features, num_features);
+S_b = zeros(num_features, num_features);
+m = mean(mean(training_data, 3), 2);
+
+for i = 1:num_classes
+    m_i = mean(training_data(:, :, i), 2);
+    S_b = S_b + (num_samples_per_class * ((m_i - m) * (m_i - m)'));
+    S_i = zeros(num_features, num_features);
+    for n = 1:num_samples_per_class
+        S_i = S_i + (training_data(:, n, i) - m_i) * (training_data(:, n, i) - m_i)';
+    end
+    S_w = S_w + S_i;
+end
+
+[V, D] = eig(pinv(S_w)*S_b);
+[~, ind] = sort(diag(D), 'descend');
+Vs = V(:, ind);
+
+transformation_matrix = Vs(:, 1:(num_classes - 1));
 
 
 
@@ -62,7 +62,7 @@ num_samples_per_class = size(training_data, 2);
 % 
 % S_b = S_t - S_w;
 % 
-% [V, D] = eig(S_b, S_w);
+% [V, D] = eig(pinv(S_w)*S_b);
 % [~, ind] = sort(diag(D), 'descend');
 % Vs = V(:, ind);
 % 
@@ -75,49 +75,49 @@ num_samples_per_class = size(training_data, 2);
 
 
 
-Sw = zeros(num_features, num_features);
-m = zeros(num_features, 1);
-
-% Compute the within class scatter matrix and total mean vector
-
-for i = 1:num_classes
-    sum = zeros(num_features, 1);
-    for n = 1:num_samples_per_class
-        sum = sum + training_data(:, n, i);
-        m = m + training_data(:, n, i);
-    end
-    m_i = sum / n;
-    
-    S_i = zeros(num_features, num_features);
-    for n = 1:num_samples_per_class
-        S_i = S_i + ((training_data(:, n, i) - m_i) * (training_data(:, n, i) - m_i)');
-    end
-    Sw = Sw + S_i;
-end
-
-m = m / (num_classes * num_samples_per_class);
-
-% Using the total mean vector, compute the total scatter matrix
-
-St = zeros(num_features, num_features);
-for i = 1:num_classes
-    for n = 1:num_samples_per_class
-        St = St + ((training_data(:, n, i) - m) * (training_data(:, n, i) - m)');
-    end
-end
-
-% The between scatter matrix is the difference of the total scatter and
-% within scatter matrices
-
-Sb = St - Sw;
-
-% Solve the generalized eigenvalue problem and select the eigenvalues of
-% the "num_classes - 1" largest eigenvalues.
-
-[V, D] = eig(Sb, Sw);
-[~, ind] = sort(diag(D), 'descend');
-Vs = V(:, ind);
-
-transformation_matrix = Vs(:, 1:(num_classes - 1));
+% Sw = zeros(num_features, num_features);
+% m = zeros(num_features, 1);
+% 
+% % Compute the within class scatter matrix and total mean vector
+% 
+% for i = 1:num_classes
+%     sum = zeros(num_features, 1);
+%     for n = 1:num_samples_per_class
+%         sum = sum + training_data(:, n, i);
+%         m = m + training_data(:, n, i);
+%     end
+%     m_i = sum / n;
+%     
+%     S_i = zeros(num_features, num_features);
+%     for n = 1:num_samples_per_class
+%         S_i = S_i + ((training_data(:, n, i) - m_i) * (training_data(:, n, i) - m_i)');
+%     end
+%     Sw = Sw + S_i;
+% end
+% 
+% m = m / (num_classes * num_samples_per_class);
+% 
+% % Using the total mean vector, compute the total scatter matrix
+% 
+% St = zeros(num_features, num_features);
+% for i = 1:num_classes
+%     for n = 1:num_samples_per_class
+%         St = St + ((training_data(:, n, i) - m) * (training_data(:, n, i) - m)');
+%     end
+% end
+% 
+% % The between scatter matrix is the difference of the total scatter and
+% % within scatter matrices
+% 
+% Sb = St - Sw;
+% 
+% % Solve the generalized eigenvalue problem and select the eigenvalues of
+% % the "num_classes - 1" largest eigenvalues.
+% 
+% [V, D] = eig(pinv(Sw)*Sb);
+% [~, ind] = sort(diag(D), 'descend');
+% Vs = V(:, ind);
+% 
+% transformation_matrix = Vs(:, 1:(num_classes - 1));
 
 end
